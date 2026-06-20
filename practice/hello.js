7ba10d7abd1b3afd86d7748aed88a3616230b83c
Colin <colina.hanks@gmail.com>
2026-06-20 15:25:01 -0600

    Remove practice

--- practice/hello.js
+++
@@ -1,7 +0,0 @@
-let name = "Konklin";
-let age = "32";
-let isLearning = true;
-
-console.log(name);
-console.log(age);
-console.log(isLearning);
--- practice/hello.py
+++
@@ -1,8 +0,0 @@
-name = "Konklin"
-age = 32
-is_learning = False
-
-print(name)
-print(age)
-print(is_learning)
-print(f"{name} is {age} years old and it is {is_learning} that he is learning Python.")
